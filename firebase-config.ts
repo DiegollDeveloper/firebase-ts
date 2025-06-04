329e93977996d71c22d0a0bc23402d273d1656bb
Diego Gonzalez <diego18_98@hotmail.com>
2025-06-03 22:09:23 -0500

    changed firebase host

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB1sd57AYqHt5BMAPbUSkkluor-V1rXDBo",
-    authDomain: "friends-88e12.firebaseapp.com",
-    projectId: "friends-88e12",
-    storageBucket: "friends-88e12.firebasestorage.app",
-    messagingSenderId: "131696718991",
-    appId: "1:131696718991:web:0c07c2a78293bef570ecfa",
-    measurementId: "G-1NZ6GVKR4T"
+    apiKey: "AIzaSyA-IS0Y9WbzuS0RRnbERlbbREt85G8SRmo",
+    authDomain: "test-poc-5e324.firebaseapp.com",
+    projectId: "test-poc-5e324",
+    storageBucket: "test-poc-5e324.firebasestorage.app",
+    messagingSenderId: "598083449749",
+    appId: "1:598083449749:web:14005bd29041841a39fc9c",
+    measurementId: "G-88W1P1DSSS"
 };
 
 // Initialize Firebase
